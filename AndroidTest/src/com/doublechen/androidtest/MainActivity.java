0b49ae404543cd2ab4b3eb23a424549f06da2812
double0291 <double0291@126.com>
2015-03-10 22:45:38 +0800

    增加ZipFile的解压方式

--- AndroidTest/src/com/doublechen/androidtest/MainActivity.java
+++ AndroidTest/src/com/doublechen/androidtest/MainActivity.java
@@ -5,8 +5,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
+
+import org.apache.http.protocol.HTTP;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -38,7 +44,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.button:
 			String dir = Environment.getExternalStorageDirectory().getPath() + File.separator + "Music"
 					+ File.separator;
-			unzip(dir + "pic.zip", dir + "pic");
+			unzip2(dir + "pic.zip", dir + "pic");
 			break;
 
 		default:
@@ -46,14 +52,20 @@ public class MainActivity extends Activity implements OnClickListener {
 		}
 	}
 
-	public void unzip(String zipFile, String destination) {
+	/**
+	 * 利用ZipInputStream解压
+	 * 
+	 * @param zipFilePath
+	 * @param destination
+	 */
+	public void unzip(String zipFilePath, String destination) {
 		try {
 			File dir = new File(destination);
 			if (!dir.isDirectory()) {
 				dir.mkdir();
 			}
 
-			ZipInputStream zin = new ZipInputStream(new FileInputStream(zipFile));
+			ZipInputStream zin = new ZipInputStream(new FileInputStream(zipFilePath));
 			try {
 				ZipEntry ze = null;
 				while ((ze = zin.getNextEntry()) != null) {
@@ -70,7 +82,7 @@ public class MainActivity extends Activity implements OnClickListener {
 						try {
 							int size;
 							byte[] buffer = new byte[BUFFER_SIZE];
-
+							// 不用buffer，一个一个字节读的话，非常非常慢
 							while ((size = zin.read(buffer, 0, BUFFER_SIZE)) != -1) {
 								out.write(buffer, 0, size);
 							}
@@ -84,6 +96,65 @@ public class MainActivity extends Activity implements OnClickListener {
 			} finally {
 				zin.close();
 			}
+			Log.d(TAG, "unzip success");
+		} catch (IOException e) {
+			Log.d(TAG, "unzip fail");
+		}
+	}
+
+	/**
+	 * 利用ZipFile解压
+	 * 
+	 * @param zipFilePath
+	 * @param destination
+	 */
+	public void unzip2(String zipFilePath, String destination) {
+		try {
+			File dir = new File(destination);
+			if (!dir.isDirectory()) {
+				dir.mkdir();
+			}
+
+			ZipFile zipFile = new ZipFile(zipFilePath);
+
+			for (Enumeration<?> entries = zipFile.entries(); entries.hasMoreElements();) {
+				ZipEntry entry = (ZipEntry) entries.nextElement();
+
+				String path = destination + File.separator + entry.getName();
+
+				if (entry.isDirectory()) {
+					File entryFile = new File(path);
+					if (!entryFile.exists()) {
+						entryFile.mkdirs();
+					}
+				} else {
+					InputStream in = zipFile.getInputStream(entry);
+					String str = new String(path.getBytes("8859_1"), HTTP.UTF_8);
+					File desFile = new File(str);
+
+					if (!desFile.exists()) {
+						File fileParentDir = desFile.getParentFile();
+						if (!fileParentDir.exists()) {
+							fileParentDir.mkdir();
+						}
+						desFile.createNewFile();
+					}
+
+					OutputStream out = new FileOutputStream(desFile);
+					try {
+						byte[] buffer = new byte[BUFFER_SIZE];
+						int size;
+						while ((size = in.read(buffer)) > 0) {
+							out.write(buffer, 0, size);
+						}
+					} finally {
+						if (in != null)
+							in.close();
+						out.close();
+					}
+				}
+			}
+
 			Log.d(TAG, "unzip success");
 		} catch (IOException e) {
 			Log.d(TAG, "unzip fail");
