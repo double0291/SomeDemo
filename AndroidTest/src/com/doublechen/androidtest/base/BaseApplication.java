702f9ae2c8b69dc4529941f0808167f932ebf88d
double0291 <double0291@126.com>
2015-06-11 16:24:21 +0800

    建立web进程

--- AndroidTest/src/com/doublechen/androidtest/base/BaseApplication.java
+++ AndroidTest/src/com/doublechen/androidtest/base/BaseApplication.java
@@ -1,13 +1,40 @@
 package com.doublechen.androidtest.base;
 
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.Application;
+import android.content.Context;
+
+import com.doublechen.androidtest.util.Logger;
 
 public class BaseApplication extends Application {
-    public static BaseApplication mApp;
+	public static BaseApplication mApp;
 
-    @Override
-    public void onCreate() {
-        mApp = this;
-        super.onCreate();
-    }
+	@Override
+	public void onCreate() {
+		mApp = this;
+		super.onCreate();
+		Logger.d(getProcessName(), true); // 每个进程启动都会走到这里
+	}
+
+	/**
+	 * 获取当前进程名
+	 */
+	public String getProcessName() {
+		List<RunningAppProcessInfo> appList = null;
+		try {
+			ActivityManager actMgr = (ActivityManager) mApp.getSystemService(Context.ACTIVITY_SERVICE);
+			appList = actMgr.getRunningAppProcesses();
+			for (RunningAppProcessInfo info : appList) {
+				if (null != info && info.pid == android.os.Process.myPid()) {
+					return info.processName;
+				}
+			}
+		} catch (Exception e) {
+			Logger.e("get process name error", false);
+		}
+		return "";
+	}
 }
